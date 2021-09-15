5de3f869ad01a9be4905cc121102e0fc006fc344
Ben Spotswood <spotswoodb@gmail.com>
2021-09-14 22:21:18 -0500

    commit tree ec2eeb3d7b9b0b150c8d11521a578f7a57b49c46

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(array) {
+    let newArray = []
+
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i] * (-1))
+    }
+
+    return newArray
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+        newArray.push(array[i] * 2)
+    }
+
+    return newArray
+}
+
+function mapToSquare(array){
+    let newArray = []
+
+    for(let i=0; i < array.length; i++){
+        newArray.push(array[i] * array[i])
+    }
+
+    return newArray
+}
+
+function reduceToTotal(array, startingPoint=0){
+    let total = startingPoint
+    for (let i=0; i < array.length; i++){
+        total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array){
+    for( let i=0; i < array.length; i++){
+        if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray){
+    for (let i=0; i<sourceArray.length; i++){
+        if (sourceArray[i]) return true
+    }
+    return false
+}
